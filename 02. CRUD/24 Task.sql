2c529bbc86ee2e3c27df0d6182c8f66deec8186c
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2025-02-15 11:25:25 +0000

    CRUD

---
+++ 02. CRUD/24 Task.sql
@@ -0,0 +1,13 @@
+CREATE VIEW view_initials
+AS SELECT
+	SUBSTRING(first_name, 1, 2) AS "initial",
+	last_name
+FROM employees
+ORDER BY last_name;
+
+-- CREATE VIEW view_initials
+-- AS SELECT
+-- 	LEFT(first_name, 2) AS "initial",
+-- 	last_name
+-- FROM employees
+-- ORDER BY last_name;
