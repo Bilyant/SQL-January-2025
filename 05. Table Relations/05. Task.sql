7add7fd1527f35dbc96fb6139e611fb0649cf920
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2025-03-20 12:22:51 +0000

    Table Relations

---
+++ 05. Table Relations/05. Task.sql
@@ -0,0 +1,21 @@
+CREATE TABLE
+	products (
+	product_name VARCHAR(100)
+	)
+;
+
+INSERT INTO
+ 	products
+VALUES
+	('Broccoli'),
+	('Shampoo'),
+	('Toothpaste'),
+	('Candy')
+;
+
+
+ALTER TABLE
+	products
+ADD COLUMN
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+;
