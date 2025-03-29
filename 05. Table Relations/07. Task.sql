18829129cf79c78a047bc3ff1dd1396ec0e159a9
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2025-03-29 12:09:34 +0000

    Table Relations

--- 05. Table Relations/07. Task.sql
+++ 05. Table Relations/07. Task.sql
@@ -1,22 +1,13 @@
 CREATE TABLE
 	passports (
 	id INT GENERATED ALWAYS AS IDENTITY (START WITH 100 INCREMENT 1) PRIMARY KEY,
-	nationatlity VARCHAR(50)
+	nationality VARCHAR(50)
 	)
 ;
 
-ALTER SEQUENCE
-	passports_id_seq
-START WITH
-	100
-INCREMENT BY
-	1
-;
-	
-
 INSERT INTO
-	passports (nationatlity)
-VALUES 
+	passports (nationality)
+VALUES
 	('N34FG21B'),
 	('K65LO4R7'),
 	('ZE657QP2' )
@@ -26,7 +17,7 @@ CREATE TABLE
 	people (
 	id SERIAL PRIMARY KEY,
 	first_name VARCHAR(50),
-	salary NUMERIC(2, 10),
+	salary NUMERIC(10, 2),
 	passport_id INT,
 
 	CONSTRAINT fk_passports
@@ -37,7 +28,7 @@ CREATE TABLE
 
 INSERT INTO
 	people (first_name, salary, passport_id)
-VALUES 
+VALUES
 	('Roberto', 43300.0000, 101),
 	('Tom', 56100.0000, 102),
 	('Yana', 60200.0000, 100)
