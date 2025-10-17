6aad16567119c408bec3c501a0554dcd174a03fa
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2025-10-17 12:19:07 +0100

    crud exercises

--- 02. CRUD/01 Task .sql
+++ 02. CRUD/01 Task .sql
@@ -1,6 +1,7 @@
 SELECT
-	*
+	id,
+	concat(first_name, ' ', last_name) AS "Full Name",
+	job_title AS "Job Title"
 FROM
-	cities
-ORDER BY
-	id;
+	employees
+;
--- 02. CRUD/06 Task.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-	id,
-	CONCAT_WS(' ', number, street) AS "assress",
-	city_id
-FROM
-	addresses
-WHERE
-	id >= 20
-;
