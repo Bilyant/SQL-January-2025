cc4d66a120d4522c10cdcbfd931aa7a001702303
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2025-11-01 10:04:09 +0000

    data aggregation

--- 04. Data Aggregation/25. Task.sql
+++ 04. Data Aggregation/25. Task.sql
@@ -4,13 +4,10 @@ CREATE TABLE
 	employee_id INT,
 	project_id INT,
 	
-	CONSTRAINT fk_employees_projects
-		FOREIGN KEY (employee_id)
-		REFERENCES employees(id),
-	CONSTRAINT fk_employees_projects	
-		FOREIGN KEY (project_id)
-		REFERENCES projects(id)
+	FOREIGN KEY (employee_id)
+	REFERENCES employees(id),
+
+	FOREIGN KEY (project_id)
+	REFERENCES projects(id)
 )
 ;
-
--- SELECT * FROM employees;
