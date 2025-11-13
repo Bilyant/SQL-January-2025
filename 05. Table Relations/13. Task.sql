516b6e367cd29b2f7d7533f7de9325f8bd102b33
Bilyant Ramiev <128568333+Bilyant@users.noreply.github.com>
2025-11-13 21:35:03 +0000

    table relations

---
+++ 05. Table Relations/13. Task.sql
@@ -0,0 +1,23 @@
+ALTER TABLE
+	countries_rivers
+ADD CONSTRAINT
+	fk_contries_rives_rivers
+FOREIGN KEY
+	(river_id)
+REFERENCES
+	rivers(id)
+ON UPDATE
+	CASCADE
+;
+
+ALTER TABLE
+	countries_rivers
+ADD CONSTRAINT
+	fk_contries_rives_countries
+FOREIGN KEY
+	(country_code)
+REFERENCES
+	countries(country_code)
+ON UPDATE
+	CASCADE
+;
